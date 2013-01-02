7a309dec4adfd9625f82b8cd2b7fd0b93d2ef946
Yuki Suga <ysuga@ysuga.net>
2013-01-02 16:31:50 +0900

    nameserver.py added

---
+++ OpenTPR/core/management/commands/nameservice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import OpenTPR
+import OpenRTM_aist
+import subprocess
+
+    
+class Command(object):
+    def __init__(self):
+        pass
+
+    def execute_with_argv(self, argv):
+        
+        pass
