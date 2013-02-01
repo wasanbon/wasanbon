715525adf8fe227649b212d52bb1605d591d6f38
Yuki Suga <ysuga@ysuga.net>
2013-02-01 18:21:49 +0900

    install setting file added.
    Signed-off-by: Yuki Suga <ysuga@ysuga.net>

--- OpenTPR/core/rtm/install_rtm.py
+++ OpenTPR/core/rtm/install_rtm.py
@@ -6,45 +6,7 @@ import platform
 import subprocess
 import zipfile
 
-rtm_dir='rtm'
-
-linux_package = {
-    'Ubuntu' : { 
-        'common': (
-            ('rm', '-rf', '/etc/apt/sources.list.d/openrtm-aist.list'),
-            ('sh', '-c', "echo \"deb http://www.openrtm.org/pub/Linux/ubuntu/ precise main\" >> /etc/apt/sources.list.d/openrtm-aist.list"), 
-            ('sh', '-c', "echo \"deb http://www.openrtm.org/pub/Linux/ubuntu/ precise-unstable main\" >> /etc/apt/sources.list.d/openrtm-aist.list"), 
-            ('apt-get', 'update')),
-        'install-cmd': ('apt-get', 'install', '-y', '--force-yes'),
-        'precise': (
-            'gcc', 'g++', 'make', 'uuid-dev', 'cmake-gui', 'doxygen', 
-            'libcv2.3', 'libcv-dev', 'libcvaux2.3', 
-            'libcvaux-dev', 'libhighgui-dev', 'libhighgui2.3',
-            'libopencv-contrib-dev', 'libopencv-contrib2.3',
-            'libomniorb4-1', 'libomniorb4-dev', 
-            'omniidl', 'omniorb-nameserver',
-            'openrtm-aist', 'openrtm-aist-dev', 
-            'openrtm-aist-example', 'openrtm-aist-doc',
-            'python-omniorb-omg', 'omniidl-python', 
-            'openrtm-aist-python', 'openrtm-aist-python-example',
-            'openjdk-7-jre', 'openjdk-7-jdk'
-            )
-        }
-    }
-
-
-rtm_java_filename = 'OpenRTM-aist-Java-1.1.0-RC1-jar.zip'
-java_package = (
-    'http://www.openrtm.org/pub/OpenRTM-aist/java/1.1.0/%s' % rtm_java_filename,  os.path.join(rtm_dir, rtm_java_filename))
-
-rtm_cpp_win_filename = 'OpenRTM-aist-1.1.0-RELEASE_vc10.msi'
-cpp_win_package = (
-    'http://www.openrtm.org/pub/Windows/OpenRTM-aist/cxx/1.1/%s' % rtm_cpp_win_filename, os.path.join(rtm_dir, rtm_cpp_win_filename))
-
-
-rtm_py_win_filename = 'OpenRTM-aist-Python-1.1.0-RC1.msi'
-py_win_package = (
-    'http://www.openrtm.org/pub/Windows/OpenRTM-aist/python/%s' % rtm_py_win_filename, os.path.join(rtm_dir, rtm_py_win_filename))
+from install_settings import *
 
 def install_rtm():
     if not os.path.isdir(rtm_dir):
@@ -66,23 +28,41 @@ def install_rtm_darwin():
     install_rtm_java()
     pass
 
+def is_cpprtm_installed_in_win():
+    return ('RTM_ROOT' in os.environ.keys())
+
+def is_pyrtm_installed_in_win():
+    try:
+        import OpenRTM_aist
+        return True
+    except ImportError, e:
+        return False
+    pass
+
 def install_rtm_win():
-    if not os.path.isfile(cpp_win_package[1]):
-        print '-Downloading OpenRTM-aist C++'
-        urllib.urlretrieve(cpp_win_package[0], cpp_win_package[1])
-    if not 'RTM_ROOT' in os.environ.keys():
+    print 'Installing OpenRTM on Windows'
+    if is_cpprtm_installed_in_win():
+        print """Your system have RTM_ROOT environemntal variable.
+                May be you've installed OpenRTM C++ version."""
+    else:
+        if not os.path.isfile(cpp_win_package[1]):
+            print '-Downloading OpenRTM-aist C++'
+            urllib.urlretrieve(cpp_win_package[0], cpp_win_package[1])
         print '-Installing OpenRTM-aist C++'
         cmd = ('msiexec', '/i', os.path.join(os.getcwd(), cpp_win_package[1]))
         subprocess.call(cmd)
-    try:
-        import OpenRTM_aist
-    except ImportError, e:
+
+    if is_pyrtm_installed_in_win():
+        print """Your system have OpenRTM_aist package in PYTHONPATH.
+        May be you've installed OpenRTM Python version."""
+    else:
         if not os.path.isfile(py_win_package[1]):
             print '-Downloading OpenRTM-aist Python'
             urllib.urlretrieve(py_win_package[0], py_win_package[1])
         print '-Installing OpenRTM-aist Python'
         cmd = ('msiexec', '/i', os.path.join(os.getcwd(), py_win_package[1]))
         subprocess.call(cmd)
+        
     install_rtm_java()
     pass
     
