a2c09983eed4717f96e984238771d13f9b1f8d0a
Yuki Suga <ysuga@ysuga.net>
2013-05-04 20:58:03 +0900

    selfupdate can git pull

--- wasanbon/core/management/commands/selfupdate.py
+++ wasanbon/core/management/commands/selfupdate.py
@@ -18,9 +18,15 @@ class Command(object):
         home_setting = yaml.load(open(os.path.join(rtm_home, 'setting.yaml'), 'r'))
         cwd = os.getcwd()
         os.chdir(rtm_temp)
-        cmd = [os.path.join(home_setting['git_path'], 'git'), 'clone', setting['common']['repository']['wasanbon']]
-        subprocess.call(cmd)
-        os.chdir('wasanbon')
+        if not os.path.isdir('wasanbon'):
+            cmd = [os.path.join(home_setting['git_path'], 'git'), 'clone', setting['common']['repository']['wasanbon']]
+            subprocess.call(cmd)
+            os.chdir('wasanbon')
+        else:
+            os.chdir('wasanbon')
+            cmd = [os.path.join(home_setting['git_path'], 'git'), 'pull']
+            subprocess.call(cmd)
+        
         cmd = ['python', 'setup.py', 'install']
         subprocess.call(cmd)
         os.chdir(cwd)
