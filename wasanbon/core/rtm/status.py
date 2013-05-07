f3e15ef99a680cf32a29a514eb52b40b026a66bc
Yuki Suga <ysuga@ysuga.net>
2013-05-08 03:28:58 +0900

    git.py added.

--- wasanbon/core/rtm/status.py
+++ wasanbon/core/rtm/status.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
-
-import wasanbon
 import os, sys
-
-from wasanbon.core.management import *
+import wasanbon
 
 def is_cpprtm_installed():
     #if platform.system() == 'Linux':
@@ -32,12 +29,6 @@ def is_pyrtm_installed():
     pass
 
 def is_javartm_installed():
-    setting = load_settings()
-    return os.path.isfile(os.path.join(setting['common']['path']['RTM_ROOT_JAVA'], setting['common']['file']['RTM_JAR']))
+    return os.path.isfile(os.path.join(wasanbon.setting['common']['path']['RTM_ROOT_JAVA'], wasanbon.setting['common']['file']['RTM_JAR']))
 
-def get_status():
-    ret = {'c++' : is_cpprtm_installed() ,
-           'python' : is_pyrtm_installed() ,
-           'java' : is_javartm_installed() }
-    return ret
 
