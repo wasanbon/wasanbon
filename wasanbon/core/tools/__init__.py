b10053e349a1d61d94b2e5367336b599efa68738
Yuki Suga <ysuga@ysuga.net>
2013-05-08 02:33:38 +0900

    git.py added.

--- wasanbon/core/tools/__init__.py
+++ wasanbon/core/tools/__init__.py
@@ -9,32 +9,21 @@ def install_tools():
     url = wasanbon.setting[sys.platform]['packages']['eclipse']
     util.download_and_unpack(url, wasanbon.rtm_home)
 
+
     try:
         import rtctree
     except ImportError, e:
         url = wasanbon.setting['common']['git']['rtctree']
-        git.clone_and_setup(url)
+        git.clone_and_setup('rtctree', url)
     try:
         import rtsprofile
     except ImportError, e:
         url = wasanbon.setting['common']['git']['rtsprofile']
-        git.clone_and_setup(url)
+        git.clone_and_setup('rtsprofile', url)
     try:
         import rtshell
     except ImportError, e:
         url = wasanbon.setting['common']['git']['rtshell']
-        git.clone_and_setup(url)
+        git.clone_and_setup('rtshell', url)
     pass
-
-def clone_and_setup(url):
-    y = yaml.load(open(os.path.join(rtm_home, 'setting.yaml'), 'r'))
-    url = setting['common']['git'][key]
-    cmd = [y['git_path'], 'clone', url, wasanbon.rtm_temp]
-    subprocess.call(command)
-
-    crrdir = os.getcwd()
-    os.chdir(os.path.join(rtm_temp, key))
-    command = ['python', 'setup.py', 'install', '--record', 'installed_files.txt']
-    subprocess.call(command)
-    os.chdir(crrdir)
 
