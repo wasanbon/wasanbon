b345010cc0f5c0dcd996efb210a4902445310672
Yuki Suga <ysuga@ysuga.net>
2013-05-08 02:34:14 +0900

    git.py added.

--- wasanbon/util/git.py
+++ wasanbon/util/git.py
@@ -6,11 +6,11 @@ def clone_and_setup(distname, url):
     #y = yaml.load(open(os.path.join(wasanbon.rtm_home, 'setting.yaml'), 'r'))
     distpath = os.path.join(wasanbon.rtm_temp, distname)
     cmd = [wasanbon.setting['local']['git'], 'clone', url, wasanbon.rtm_temp, distpath]
-    subprocess.call(command)
+    subprocess.call(cmd)
 
     crrdir = os.getcwd()
     os.chdir(distpath)
-    command = ['python', 'setup.py', 'install', '--record', 'installed_files.txt']
-    subprocess.call(command)
+    cmd = ['python', 'setup.py', 'install', '--record', 'installed_files.txt']
+    subprocess.call(cmd)
     os.chdir(crrdir)
 
