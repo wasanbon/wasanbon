d8ab8b5a6ee00627154cffb80fcc4f66c6e092cc
Yuki Suga <ysuga@ysuga.net>
2013-05-18 12:46:22 +0900

    mgr.py template udpated.

--- wasanbon/template/mgr.py
+++ wasanbon/template/mgr.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if sys.platform == 'win32':
+    import codecs
+    sys.stdout = codecs.getwriter('cp932')(sys.stdout)
+
 
 import wasanbon
 from wasanbon.core import management
