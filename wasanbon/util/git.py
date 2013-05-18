039a8ab43f398aae830c04e7e9c86e7a91859f4c
Yuki Suga <ysuga@ysuga.net>
2013-05-18 11:31:20 +0900

    git clone updated.

--- wasanbon/util/git.py
+++ wasanbon/util/git.py
@@ -5,7 +5,7 @@ import wasanbon
 def clone_and_setup(url):
     distpath = os.path.join(wasanbon.rtm_temp, os.path.basename(url)[:-4])
     if not 'local' in wasanbon.setting.keys():
-        wasanbon.setting['local'] = yaml.load(open(os.path.join(rtm_home, 'setting.yaml'), 'r'))
+        wasanbon.setting['local'] = yaml.load(open(os.path.join(wasanbon.rtm_home, 'setting.yaml'), 'r'))
 
     cmd = [wasanbon.setting['local']['git'], 'clone', url, distpath]
     print cmd
