d5827a4c21254381226444125e2dbb925c5eddf0
Yuki Suga <ysuga@ysuga.net>
2013-05-20 10:36:34 +0900

    packages.yaml for linux2 updated.

--- wasanbon/core/rtm/__init__.py
+++ wasanbon/core/rtm/__init__.py
@@ -25,26 +25,26 @@ def install_rtm(force=False):
     if sys.platform == 'linux2':
         srcsfile = '/etc/apt/sources.list.d/openrtm-aist.list'
         key1 = 'deb http://www.openrtm.org/pub/Linux/ubuntu/ precise main\n'
-        key2 = 'deb http://www.openrtm.org/pub/Linux/ubuntu/ precise-unstable main'
+        #key2 = 'deb http://www.openrtm.org/pub/Linux/ubuntu/ precise-unstable main'
         if os.path.isfile(srcsfile):
             flag1 = False
-            flag2 = False
+            #flag2 = False
             file = open(srcsfile, 'r+w')
             for line in file:
                 if line.strip() == key1:
                     flag1 = True
-                if line.strip() == key2:
-                    flag2 = True
+                #if line.strip() == key2:
+                #    flag2 = True
                     
             if not flag1:
                 file.write(key1)
-            if not flag2:
-                file.write(key2)
+            #if not flag2:
+            #    file.write(key2)
             file.close()
         else:
             file = open(srcsfile, 'w')
             file.write(key1)
-            file.write(key2)
+            #file.write(key2)
             file.close()
 
         subprocess.call(['apt-get', 'update'])
