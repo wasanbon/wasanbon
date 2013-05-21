5cd5b5d1dab9f6d30794d2960a32e4112b122105
Yuki Suga <ysuga@ysuga.net>
2013-05-21 21:36:14 +0900

    system run updated.

--- wasanbon/core/management/commands/system.py
+++ wasanbon/core/management/commands/system.py
@@ -4,13 +4,6 @@ from wasanbon.core import rtc
 from wasanbon.core import system
 from wasanbon.core.system import run
 
-def signal_action(num, frame):
-    print 'SIGINT captured'
-    global endflag
-    endflag = True
-    pass
-
-endflag = False
 
 class Command(object):
     def __init__(self):
@@ -32,46 +25,10 @@ class Command(object):
                     rtc.install(rtcp)
 
         elif(argv[2] == 'run'):
-            sys.stdout.write('Ctrl+C to stop system.\n')
-            signal.signal(signal.SIGINT, signal_action)
-
-            sys.stdout.write('Starting RTC-Daemons\n')
             if len(argv) >= 4 and argv[3] == '--nobuild':
                 sys.stdout.write('\n - Launch System without System Build.\n\n')
                 nobuild=True
             else:
                 nobuild=False
-            system.run_system(argv, nobuild=nobuild)
 
-            global endflag
-            
-            if not nobuild:
-                interval = 3
-                for i in range(0, interval):
-                    sys.stdout.write('\rwaiting %s seconds to rebuild RTSystem.' % (interval-i))
-                    sys.stdout.flush()
-                    time.sleep(1)
-
-                while not endflag:
-                    sys.stdout.write('\n rtresurrect.\n')                    
-                    if run.exe_rtresurrect():
-                        time.sleep(1)
-                        break
-                while not endflag:
-                    sys.stdout.write('\n rtstart.\n')
-                    if run.exe_rtresurrect():
-                        time.sleep(1)
-                        break
-            sys.stdout.write('System successfully started.\n')
-            while not endflag:
-                try:
-                    time.sleep(0.1)
-                    if system.is_all_process_terminated():
-                        break
-                except Exception, e:
-                    print 'Exception Occurred: %s' % repr(e)
-                    endflag = True
-                    pass
-            print 'Terminating All Process....'        
-            system.terminate_all_process()
-            print 'All rtcd process terminated.'
+            system.run_system(nobuild=nobuild)
--- wasanbon/core/system/__init__.py
+++ wasanbon/core/system/__init__.py
@@ -1,10 +1,18 @@
 # encoding: UTF-8
 
-import os, sys, time
+import os, sys, time, signal
 import wasanbon
 from wasanbon.core.system import run
 
 process = {}
+
+def signal_action(num, frame):
+    print 'SIGINT captured'
+    global endflag
+    endflag = True
+    pass
+
+endflag = False
 
 def is_all_process_terminated():
     flags = []
@@ -19,12 +27,12 @@ def is_all_process_terminated():
             else:
                 flags.append(True)
     if len(flags) != 0:
-        print flags
         return all(flags)
     else:
         return False
 
-def run_system(argv, nobuild):
+
+def start_process():
     if not os.path.isdir('log'):
         os.mkdir('log')
 
@@ -33,6 +41,47 @@ def run_system(argv, nobuild):
     process['python'] = run.start_python_rtcd()
     process['java']   = run.start_java_rtcd()
 
+
+def run_system(nobuild):
+    sys.stdout.write('Ctrl+C to stop system.\n')
+    signal.signal(signal.SIGINT, signal_action)
+
+    sys.stdout.write('Starting RTC-Daemons\n')
+    
+    start_process()
+
+    global endflag
+    
+    if not nobuild:
+        interval = 3
+        for i in range(0, interval):
+            sys.stdout.write('\rwaiting %s seconds to rebuild RTSystem.' % (interval-i))
+            sys.stdout.flush()
+            time.sleep(1)
+            
+        while not endflag:
+            sys.stdout.write('\n rtresurrect.\n')                    
+            if run.exe_rtresurrect():
+                time.sleep(1)
+                break
+        while not endflag:
+            sys.stdout.write('\n rtstart.\n')
+            if run.exe_rtresurrect():
+                time.sleep(1)
+                break
+    sys.stdout.write('System successfully started.\n')
+    while not endflag:
+        try:
+            time.sleep(0.1)
+            if is_all_process_terminated():
+                break
+        except Exception, e:
+            print 'Exception Occurred: %s' % repr(e)
+            endflag = True
+            pass
+    print 'Terminating All Process....'        
+    terminate_all_process()
+    print 'All rtcd process terminated.'
 
 
 def terminate_all_process():
