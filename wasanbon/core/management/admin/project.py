33ba55477744c34af70fc3ff252d2b6453fc99e9
ysuga <ysuga@ysuga.net>
2013-06-10 13:34:13 +0900

    wasanbon-admin.py project updated. list command added. register workspace when created.
    Signed-off-by: ysuga <ysuga@ysuga.net>

--- wasanbon/core/management/admin/project.py
+++ wasanbon/core/management/admin/project.py
@@ -10,11 +10,14 @@ class Command(object):
         return True
 
     def execute_with_argv(self, argv):
-        if len(argv) < 4:
+        if len(argv) < 3:
             wasanbon.show_help_description('project')
             return
-        if argv[3] == 'help':
+        if argv[2] == 'help':
             wasanbon.show_help_description('project')
             return
+        if argv[2] == 'start':
+            init_workspace(argv[3])
+        elif argv[2] == 'list':
+            list_workspace()
 
-        init_workspace(argv[3])
