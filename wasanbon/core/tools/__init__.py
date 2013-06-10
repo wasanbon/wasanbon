7d6c37d9fdd00488dfb5db9eb6b54fe84ef30c77
ysuga <ysuga@ysuga.net>
2013-06-10 12:29:58 +0900

    force ooption for wasanbon-admin.py init is added.
    Signed-off-by: ysuga <ysuga@ysuga.net>

--- wasanbon/core/tools/__init__.py
+++ wasanbon/core/tools/__init__.py
@@ -14,23 +14,32 @@ def install_tools(force=False):
     except ImportError, e:
         sys.stdout.write('OpenRTM_aist can not be imported. Please install RTM first.\n')
         return
-
-    try:
-        import rtctree
-    except ImportError, e:
+    
+    if force:
         url = wasanbon.setting['common']['git']['rtctree']
         git.clone_and_setup(url)
-    try:
-        import rtsprofile
-    except ImportError, e:
         url = wasanbon.setting['common']['git']['rtsprofile']
         git.clone_and_setup(url)
-    try:
-        import rtshell
-    except ImportError, e:
         url = wasanbon.setting['common']['git']['rtshell']
         git.clone_and_setup(url)
-    pass
+
+    else:
+        try:
+            import rtctree
+        except ImportError, e:
+            url = wasanbon.setting['common']['git']['rtctree']
+            git.clone_and_setup(url)
+        try:
+            import rtsprofile
+        except ImportError, e:
+            url = wasanbon.setting['common']['git']['rtsprofile']
+            git.clone_and_setup(url)
+        try:
+            import rtshell
+        except ImportError, e:
+            url = wasanbon.setting['common']['git']['rtshell']
+            git.clone_and_setup(url)
+        pass
 
     url = wasanbon.setting[sys.platform]['packages']['eclipse']
     util.download_and_unpack(url, wasanbon.rtm_home, force)
