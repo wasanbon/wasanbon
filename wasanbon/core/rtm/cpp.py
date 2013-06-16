a1031a8db80e944cd223c94c017a555ae20ea394
Yuki Suga <ysuga@ysuga.net>
2013-06-16 12:49:22 +0900

    selfupdate updated.

---
+++ wasanbon/core/rtm/cpp.py
@@ -0,0 +1,89 @@
+import wasanbon
+import os, sys
+
+def is_installed():
+    if sys.platform == 'darwin':
+        file = 'version.txt'
+        path = '/usr/local/include/openrtm-1.1/rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        path = '/usr/include/openrtm-1.1/rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        if ('RTM_ROOT' in os.environ.keys()):
+            if os.path.isfile(os.environ['RTM_ROOT'], 'rtm', file):
+                return True
+        return False
+
+    elif sys.platform == 'linux2':
+        file = 'version.txt'
+        path = '/usr/local/include/openrtm-1.1/rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        path = '/usr/include/openrtm-1.1/rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        if ('RTM_ROOT' in os.environ.keys()):
+            if os.path.isfile(os.environ['RTM_ROOT'], 'rtm', file):
+                return True
+        return False
+
+    elif sys.platform == 'win32':
+        file = 'version.txt'
+        path = 'C:\\Program Files (x86)\\OpenRTM-aist\\1.1\\rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        path = 'C:\\Program Files\\OpenRTM-aist\\1.1\\rtm'
+        if os.path.isfile(os.path.join(path, file)):
+            return True
+        if ('RTM_ROOT' in os.environ.keys()):
+            if os.path.isfile(os.environ['RTM_ROOT'], 'rtm', file):
+                return True
+        return False
+
+    else:
+        raise wasanbon.UnsupportedPlatformError()
+
+    return False
+
+
+
+def install(force=False):
+    if is_installed() and not force:
+        print " - Your system seems to have OpenRTM C++."
+        return False
+
+    if sys.platform == 'darwin':
+        install_cpprtm_osx(force)
+    elif sys.platform == 'win32':
+        install_cpprtm_win(force)
+    elif sys.platform == 'linux2':
+        install_cpprtm_linux(force)
+    pass
+
+
+def install_cpprtm_win(force):
+    util.download_and_install(wasanbon.setting['win32']['packages']['c++'], force=force)
+
+def install_cpprtm_linux(force):
+    util.download_and_install(wasanbon.setting['linux2']['packages']['c++_ppa'], force=force)
+
+def install_cpprtm_osx(force):
+    util.download_and_install(wasanbon.setting['darwin']['packages']['c++'], force=force)
+    srcdir = '/usr/local/lib/python2.7/site-packages' 
+    distdir = os.path.split(wasanbon.__path__[0])[0]
+    for file in os.listdir(srcdir):
+        filepath = os.path.join(srcdir, file)
+        distpath = os.path.join(distdir, file)
+        if os.path.isfile(filepath):
+            if os.path.isfile(distpath) and force:
+                os.remove(distpath)
+            if not os.path.isfile(distpath):
+                shutil.copy2(filepath, distpath)
+        elif os.path.isdir(filepath):
+            if os.path.isdir(distpath) and force:
+                shutil.rmtree(distpath)
+            if not os.path.isdir(distpath):
+                shutil.copytree(os.path.join(srcdir, file), os.path.join(distdir, file))
+    pass
+
---
+++ wasanbon/core/rtm/python.py
@@ -0,0 +1,53 @@
+import os, sys, shutil, subprocess
+import wasanbon
+
+def is_installed():
+    try:
+        import OpenRTM_aist
+        return True
+    except ImportError, e:
+        return False
+    pass
+
+
+def install(force=False):
+    if is_installed() and not force:
+        print " - Your system has OpenRTM_aist package in PYTHONPATH."
+        return False
+    if sys.platform == 'darwin':
+        install_pyrtm_osx(force)
+    elif sys.platform == 'win32':
+        install_pyrtm_win(force)
+    elif sys.platform == 'linux2':
+        install_pyrtm_linux(force)
+    pass
+
+
+def install_pyrtm_win(force):
+    util.download_and_install(wasanbon.setting['win32']['packages']['python'], force=force)
+
+
+def install_pyrtm_linux(force):
+    util.download_and_install(wasanbon.setting['linux2']['packages']['python_ppa'], force=force)
+    #install_pyrtm_osx(force) # install from source.
+
+def install_pyrtm_osx(force):
+    if not 'local' in wasanbon.setting.keys():
+        wasanbon.setting['local'] = yaml.load(open(os.path.join(wasanbon.rtm_home, 'setting.yaml'), 'r'))
+
+    old_dir = os.getcwd()
+    os.chdir(wasanbon.rtm_temp)
+    reponame = wasanbon.setting['common']['svn']['python']
+    cmd = [wasanbon.setting['local']['svn'], 'co', reponame]
+    ret = subprocess.call(cmd)
+    if reponame.endswith('/'):
+        reponame = reponame[:-1]
+    dirname = os.path.basename(reponame)
+    os.chdir(dirname)
+    cmd = ['python', 'setup.py', 'build_core']
+    ret = subprocess.call(cmd)
+    cmd = ['python', 'setup.py', 'install']
+    ret = subprocess.call(cmd)
+    cmd = ['python', 'setup.py', 'install_example']
+    ret = subprocess.call(cmd)
+    os.chdir(old_dir)
