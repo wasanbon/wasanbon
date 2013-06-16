dfe8dc00a250b44b8cd904af7b0201881d0c3e96
Yuki Suga <ysuga@ysuga.net>
2013-06-16 17:22:46 +0900

    temp commit

--- wasanbon/core/management/admin/selfupdate.py
+++ wasanbon/core/management/admin/selfupdate.py
@@ -4,36 +4,69 @@ import wasanbon
 import os, sys, subprocess, shutil
 from wasanbon import util
 
+
+def pull_and_update(verbose):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(wasanbon.rtm_temp, 'wasanbon'))
+
+    if verbose:
+        sys.stdout.write(' - Pulling from %s\n' % wasanbon.setting['common']['repository']['wasanbon']['git'])
+        pass
+
+    cmd = [wasanbon.setting['local']['git'], 'pull']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output = p.stdout.readline()
+    if output.strip() == 'Already up-to-date.':
+        sys.stdout.write(output)
+        return
+
+    install(verbose)
+
+def clone_and_update(verbose):
+    if verbose: 
+        sys.stdout.write(' - Cloning %s\n' % wasanbon.setting['common']['repository']['wasanbon']['git'])
+        pass
+
+    cmd = [wasanbon.setting['local']['git'], 
+           'clone', 
+           wasanbon.setting['common']['repository']['wasanbon']['git']]
+    subprocess.call(cmd)
+    os.chdir('wasanbon')
+    pass
+
+def cleanup(verbose):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(wasanbon.rtm_temp, 'wasanbon'))
+    if verbose:
+        sys.stdout.write(' - Cleanly uninstalled.\n')
+    shutil.rmtree(wasanbon.__path__[0])
+    shutil.rmtree('build')
+    os.chdir(cwd)
+    pass
+
+def install(verbose):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(wasanbon.rtm_temp, 'wasanbon'))
+    cmd = ['python', 'setup.py', 'install']
+    subprocess.call(cmd)
+    os.chdir(cwd)
+    pass
+
 class Command(object):
     def __init__(self):
         pass
 
     def execute_with_argv(self, argv, verbose=False, force=False, clean=False):
         cwd = os.getcwd()
-        #if not os.path.isdir(wasanbon.rtm_temp):
-        #    os.umask(0000)
-        #    os.mkdir(wasanbon.rtm_temp, 0777)
+        if verbose:
+            sys.stdout.write(' - Changing directory to %s\n' % wasanbon.rtm_temp)
         os.chdir(wasanbon.rtm_temp)
 
         if not os.path.isdir('wasanbon'):
-            cmd = [wasanbon.setting['local']['git'], 
-                   'clone', 
-                   wasanbon.setting['common']['repository']['wasanbon']['git']]
-            subprocess.call(cmd)
-            os.chdir('wasanbon')
         else:
-            os.chdir('wasanbon')
-            cmd = [wasanbon.setting['local']['git'],
-                   'pull']
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            output = p.stdout.readline()
-            if output.strip() == 'Already up-to-date.':
-                sys.stdout.write(output)
-                os.chdir(cwd)
-                return 0
+
         if clean:
-            shutil.rmtree(wasanbon.__path__[0])
-            shutil.rmtree('build')
-        cmd = ['python', 'setup.py', 'install']
-        subprocess.call(cmd)
+            cleanup(verbose)
+        else:
+            install(verbose)
         os.chdir(cwd)
