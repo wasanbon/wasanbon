b7d00d6864dc94e8f4bf323d282900871025819c
Yuki Suga <ysuga@ysuga.net>
2013-06-16 12:48:50 +0900

    selfupdate updated.

--- wasanbon/core/management/admin/selfupdate.py
+++ wasanbon/core/management/admin/selfupdate.py
@@ -8,26 +8,11 @@ class Command(object):
     def __init__(self):
         pass
 
-    def is_admin(self):
-        return True
-
-    def execute_with_argv(self, argv):
-        clean_flag = False
-        if len(argv) >= 3 and argv[2] == 'help':
-            wasanbon.show_help_description('selfupdate')
-            return
-
-        elif len(argv) >= 3 and argv[2] == '--clean':
-            if not util.yes_no('Do you really want to cleanup and update wasanbon/') == 'yes':
-                print 'Aborted.'
-                return
-            else:
-                clean_flag = True
-
+    def execute_with_argv(self, argv, verbose=False, force=False, clean=False):
         cwd = os.getcwd()
-        if not os.path.isdir(wasanbon.rtm_temp):
-            os.umask(0000)
-            os.mkdir(wasanbon.rtm_temp, 0777)
+        #if not os.path.isdir(wasanbon.rtm_temp):
+        #    os.umask(0000)
+        #    os.mkdir(wasanbon.rtm_temp, 0777)
         os.chdir(wasanbon.rtm_temp)
 
         if not os.path.isdir('wasanbon'):
@@ -46,8 +31,9 @@ class Command(object):
                 sys.stdout.write(output)
                 os.chdir(cwd)
                 return 0
-        if clean_flag:
+        if clean:
             shutil.rmtree(wasanbon.__path__[0])
+            shutil.rmtree('build')
         cmd = ['python', 'setup.py', 'install']
         subprocess.call(cmd)
         os.chdir(cwd)
