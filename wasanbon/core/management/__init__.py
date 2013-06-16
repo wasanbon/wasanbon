2d1286502384889b89807dcf757f141b6348327b
Yuki Suga <ysuga@ysuga.net>
2013-06-16 11:28:48 +0900

    wasanbon-admin.py and mgr.py updated.

--- wasanbon/core/management/__init__.py
+++ wasanbon/core/management/__init__.py
@@ -1,16 +1,2 @@
-#!/usr/bin/env python
-
-import sys, os
-
-__path = os.path.join(os.path.dirname(__file__), "TPR_CORE_PATH")
-if __path not in sys.path:
-    sys.path.append(__path)
-del __path
 
 
-def import_module(name):
-    __import__(name)
-    return sys.modules[name]
-
-
-import application
