9276c4f1600b9f4e37ccf225d628891cd155cef4
ysuga <ysuga@ysuga.net>
2013-06-17 10:47:05 +0900

    mgr.py updated.

--- wasanbon/core/management/commands/admin.py
+++ wasanbon/core/management/commands/admin.py
@@ -13,14 +13,15 @@ class Command(object):
     def is_admin(self):
         return False
 
-    def execute_with_argv(self, argv):
+    def execute_with_argv(self, argv, verbose, clean, force):
         if len(argv) < 3:
-            wasanbon.show_help_description('admin')
+            print 'To read help, input "mgr.py admin help"'
             return
+
         if argv[2] == 'register':
             appname = os.path.basename(os.getcwd())
             appdir  = os.getcwd()
             print ' - Trying to register %s to %s' % (appname, os.path.join(wasanbon.rtm_home, "workspace.yaml"))
             
-            register_workspace(appname, appdir)
+            register_workspace(appname, appdir, verbose=verbose)
 
