54ab5515c1f36e82f7c5e315b01f92c264103528
Yuki Suga <ysuga@ysuga.net>
2013-06-17 21:09:45 +0900

    admin.py init

--- wasanbon/core/management/commands/admin.py
+++ wasanbon/core/management/commands/admin.py
@@ -10,10 +10,7 @@ class Command(object):
     def __init__(self):
         pass
 
-    def is_admin(self):
-        return False
-
-    def execute_with_argv(self, argv):
+    def execute_with_argv(self, argv, force, verbose, clean):
         if len(argv) < 3:
             wasanbon.show_help_description('admin')
             return
