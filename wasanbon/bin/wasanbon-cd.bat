a886fb7b3629085ea606fdbc02687a330bc32cfd
ysuga <ysuga@ysuga.net>
2013-08-02 01:22:50 +0900

    wasanbon-cd updated.

--- wasanbon/bin/wasanbon-cd.bat
+++ wasanbon/bin/wasanbon-cd.bat
@@ -1,2 +1,7 @@
+@if "%1" == "" goto list
 @FOR /f "DELIMS=" %%A IN ('wasanbon-admin.py project directory %1') DO @SET TARGET_DIR=%%A
 @cd %TARGET_DIR%
+@goto end
+:list
+@wasanbon-admin.py project list
+:end
