1d62d9466583f40418a22ae97af29525f3c7c060
ysuga <ysuga@ysuga.net>
2013-08-07 00:39:12 +0900

    git clone updated.

--- wasanbon/core/rtc/__init__.py
+++ wasanbon/core/rtc/__init__.py
@@ -6,15 +6,21 @@ from repository import *
 from rtc_object import *
 
 
-class RTC():
-    
-    def __init__(self, path):
-        self._profile = None
-        pass
+def get_repositories(verbose=False):
+    repos = []
+    for name, value in wasanbon.repositories.items():
+        if 'git' in value.keys():
+            protocol = 'git'
+        elif 'hg' in value.keys():
+            protocol = 'hg'
+        repos.append(RtcRepository(name=name, url=value[protocol], desc=value['description'], hash="", protocol=protocol))
+    return repos
 
-    def build(self):
-        pass
-    
-    @property
-    def profile(self):
-        return self._profile
+def get_repository(name, verbose=False):
+    repos = get_repositories(verbose=verbose)
+    for repo in repos:
+        if repo.name == name:
+            return repo
+
+    return None
+
