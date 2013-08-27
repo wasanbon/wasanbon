d34c5a964020911af5ebbc098a7734afc05b2da8
ysuga <ysuga@ysuga.net>
2013-08-27 10:41:01 +0900

    update clone_and setup

--- wasanbon/util/git/__init__.py
+++ wasanbon/util/git/__init__.py
@@ -1,9 +1,37 @@
-import sys, os, subprocess
+import sys, os, subprocess, yaml
+import shutil
+import wasanbon
 
 from github_obj  import *
 from git_obj import *
 
-import wasanbon
+def clone_and_setup(url, verbose=False, force=False):
+    distpath = os.path.join(wasanbon.rtm_temp, os.path.basename(url)[:-4])
+    if not 'local' in wasanbon.setting.keys():
+        wasanbon.setting['local'] = yaml.load(open(os.path.join(wasanbon.rtm_home, 'setting.yaml'), 'r'))
+
+    stdout = None if verbose else subprocess.PIPE
+
+    if os.path.isdir(distpath):
+        print ' - Path (%s) is existing' % distpath
+        if force:
+            print ' - Force to install'
+            if verbose:
+                print ' - Removing Path (%S)' % distpath
+            shutil.rmtree(distpath)
+
+    cmd = [wasanbon.setting['local']['git'], 'clone', url, distpath]
+    if verbose:
+        print ' - Cloning %s' % url
+    subprocess.call(cmd, stdout=stdout)
+    crrdir = os.getcwd()
+    os.chdir(distpath)
+    cmd = ['python', 'setup.py', 'install', '--record', 'installed_files.txt']
+    if verbose:
+        print ' - Install (setup.py install) in %s' % distpath
+    subprocess.call(cmd, stdout=stdout)
+    os.chdir(crrdir)
+
 
 def git_command(commands, path='.', verbose = False, pipe=False):
     cur_dir = os.getcwd()
