2d8bab60624db8b1a123a5841153c123415c61d5
ysuga <ysuga@ysuga.net>
2013-09-02 14:17:24 +0900

    tools rtse updated.

--- wasanbon/core/management/commands/tools.py
+++ wasanbon/core/management/commands/tools.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, time
 
 import wasanbon
 from wasanbon.core import tools 
@@ -11,16 +11,19 @@ from rtshell import rtcryo
 def save_all_system(nameservers, filepath='system/DefaultSystem.xml', verbose=False):
     if verbose:
         sys.stdout.write(" - Saving System on %s to %s\n" % (str(nameservers), filepath))
-    try:
-        argv = ['--verbose', '-n', 'DefaultSystem01', '-v', '1.0', '-e', 'Sugar Sweet Robotics',  '-o', filepath]
-        argv = argv + nameservers
-        rtcryo.main(argv=argv)
-    except omniORB.CORBA.UNKNOWN, e:
-        traceback.print_exc()
-        pass
-    except Exception, e:
-        traceback.print_exc()
-        return False
+    for i in range(0, 5):
+        try:
+            argv = ['--verbose', '-n', 'DefaultSystem01', '-v', '1.0', '-e', 'Sugar Sweet Robotics',  '-o', filepath]
+            argv = argv + nameservers
+            rtcryo.main(argv=argv)
+            sys.stdout.write(' - Saved.\n')
+            return
+        except omniORB.CORBA.UNKNOWN, e:
+            #traceback.print_exc()
+            pass
+        except Exception, e:
+            pass
+
 
 class Command(object):
     def __init__(self):
@@ -60,15 +63,38 @@ class Command(object):
                 if not ns.check_and_launch(verbose=verbose, force=force):
                     sys.stdout.write(' @ NameService %s is not running\n' % ns.path)
                     return False
+
+            for i in range(0, 5):
+                sys.stdout.write('\r - Waiting (%s/%s)\n' % (i+1, 5))
+                sys.stdout.flush()
+                time.sleep(1)
+
             
             proj.launch_all_rtcd(verbose=verbose)
-            tools.launch_eclipse(proj.system_path, nonblock=False, verbose=verbose)
+            #tools.launch_eclipse(proj.system_path, nonblock=False, verbose=verbose)
+            tools.launch_eclipse(nonblock=False, verbose=verbose)
+
+            for i in range(0, 5):
+                sys.stdout.write('\r - Waiting (%s/%s)\n' % (i+1, 5))
+                sys.stdout.flush()
+                time.sleep(1)
+
+            for ns in nss:
+                ns.refresh(verbose=verbose, force=True)
+                
+            #pairs = proj.available_connection_pairs(nameservers=nss, verbose=verbose)
             
             ns_addrs = [ns.path for ns in nss]
             save_all_system(['localhost'])
+
+            for i in range(0, 5):
+                sys.stdout.write('\r - Waiting (%s/%s)\n' % (i+1, 5))
+                sys.stdout.flush()
+                time.sleep(1)
+
             proj.terminate_all_rtcd(verbose=verbose)
             
             for ns in nss:
                 ns.kill()
-
-        raise wasanbon.InvalidUsageException()
+        else:
+            raise wasanbon.InvalidUsageException()
