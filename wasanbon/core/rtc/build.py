669ed9af804d8ade5bc23278d3a369a4f5be9f55
Yuki Suga <ysuga@ysuga.net>
2013-10-31 15:55:20 +0900

    update cleanup script for java

--- wasanbon/core/rtc/build.py
+++ wasanbon/core/rtc/build.py
@@ -197,3 +197,21 @@ def clean_rtc_cpp(rtcp, verbose=False):
                     print ' - Removing Emacs backup file %s' % fullpath
                 os.remove(os.path.join(root, file))
 
+
+def clean_rtc_java(rtcp, verbose=False):
+    rtc_dir, rtc_xml = os.path.split(rtcp.filename)
+    build_dir = os.path.join(rtc_dir, 'build-' + sys.platform)
+    if os.path.isdir(build_dir):
+        if verbose:
+            print ' - Removing Building Directory %s' % build_dir
+        shutil.rmtree(build_dir, ignore_errors=True )
+    for root, dirs, files in os.walk(rtc_dir):
+        for file in files:
+            if file.endswith('~'):
+                fullpath = os.path.join(root, file)
+                if fullpath.startswith(os.getcwd()):
+                    fullpath = fullpath[len(os.getcwd())+1]
+                if verbose:
+                    print ' - Removing Emacs backup file %s' % fullpath
+                os.remove(os.path.join(root, file))
+
