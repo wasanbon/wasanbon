262af8b73b0b64e21213de8edd86ee1c35db7f80
ysuga <ysuga@ysuga.net>
2014-01-10 19:20:31 +0900

    proxy command added.

---
+++ wasanbon/core/management/admin/proxy.py
@@ -0,0 +1,19 @@
+import os, sys, yaml, shutil, subprocess, traceback, optparse
+if sys.platform == 'darwin' or sys.platform == 'linux2':
+    import pwd, grp
+import wasanbon
+from wasanbon import util
+
+class Command(object):
+    def __init__(self):
+        pass
+
+    def execute_with_argv(self, argv, force=False, verbose=False, clean=False):
+        if len(argv) >= 3:
+            host, port = argv[2].split(':')
+            util.set_proxy(host, port, verbose=True)
+            return
+        else:
+            util.omit_proxy(verbose=True)
+            return
+
