da13fc2b949cbeed23ace3dfeda9c4e7049b8f25
Yuki Suga <ysuga@ysuga.net>
2015-07-27 19:11:21 +0900

    update for pip distribution

--- wasanbon/__init__.py
+++ wasanbon/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+_version = 1.0.0b4
 
 import sys, os, locale, getpass, time #, yaml
 import platform as plt
@@ -10,7 +11,7 @@ import datetime
 def get_version():
     """Get wasanbon version.
     """
-    return "1.0.0"
+    return _version
 
 
 IDE = 'Visual Studio 12' if sys.platform == 'win32' else 'Makefile'
