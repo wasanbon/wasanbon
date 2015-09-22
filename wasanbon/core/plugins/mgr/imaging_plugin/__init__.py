c0e73e13925d634fd12a3177ce984987f9d9f52b
ysuga <ysgua@ysuga.net>
2015-09-22 14:07:20 +0900

    create_rtc_image plugin added

---
+++ wasanbon/core/plugins/mgr/imaging_plugin/__init__.py
@@ -0,0 +1,180 @@
+import os, sys
+import wasanbon
+from wasanbon.core.plugins import PluginFunction, manifest
+
+class Plugin(PluginFunction):
+
+    def __init__(self):
+        #PluginFunction.__init__(self)
+        super(Plugin, self).__init__()
+        pass
+
+    def depends(self):
+        return ['admin.environment']
+
+    def get_num_ports(self, rtcprof):
+        len_svc_intf = 0
+        for s in rtcprof.serviceports:
+            len_svc_intf = len_svc_intf + len(s.serviceInterfaces)
+            pass
+        num_rightside_port = len(rtcprof.outports) + len(rtcprof.serviceports) + len_svc_intf
+        num_leftside_port = len(rtcprof.inports)
+        if num_rightside_port > num_leftside_port:
+            num_port = num_rightside_port
+        else:
+            num_port = num_leftside_port
+            pass
+        return num_port, num_rightside_port, num_leftside_port
+
+    def get_image_height(self, rtcprof, port_height = 20):
+        (num_port, num_rightside_port, num_leftside_port) = self.get_num_ports(rtcprof)
+        top_margin = port_height
+        bottom_margin = port_height 
+        top_bottom_margin = port_height
+        port_margin = port_height
+        num_margin = num_port -1
+        if num_margin < 0: num_margin = 0
+        img_height = num_port * port_height + num_margin * port_margin + top_bottom_margin * 2 + top_margin + bottom_margin
+        img_width  = 800
+        return img_height
+
+    def get_image(self, rtcprof, port_height=20, 
+                  fill_color=(20,20,255,255), line_color=(15,15,30,255), background_color=(228,212,162,0)):
+        from PIL import Image
+        img_width = 800
+        img_height = self.get_image_height(rtcprof, port_height)
+        img_height = img_height + port_height # for text
+
+        im = Image.new('RGBA', (img_width, img_height), background_color)
+        top_margin = port_height
+        rtc_width  = port_height * 5
+        rtc_topleft = ( (img_width - rtc_width) / 2, top_margin)
+        
+        self.write_rtc_image(im, rtcprof, rtc_topleft, port_height, line_color, fill_color)
+        return im
+
+
+    def write_rtc_image(self, im, rtcprof, rtc_topleft, port_height, line_color, fill_color):
+        from PIL import Image, ImageDraw, ImageFont
+        img_width = im.width
+        draw = ImageDraw.Draw(im)
+
+        (num_port, num_rightside_port, num_leftside_port) = self.get_num_ports(rtcprof)
+        num_margin = num_port -1
+        if num_margin < 0: num_margin = 0
+
+        port_margin = port_height
+        top_margin = port_height
+        top_bottom_margin = port_height
+        rtc_height = num_port * port_height + num_margin * port_margin + top_bottom_margin *2
+        rtc_width  = port_height * 5
+        rtc_rightbottom = (rtc_topleft[0] + rtc_width, top_margin + rtc_height)
+
+        draw.rectangle((rtc_topleft, rtc_rightbottom), fill=fill_color, outline=line_color)
+
+        # For FONT
+        text_color = line_color
+        if sys.platform == 'win32':
+            font_path = "C:\\Windows\\Fonts\\cour.ttf"
+        else:
+            font_path = "/Library/Fonts/Courier New.ttf"
+            pass
+        text_font = ImageFont.truetype(font_path, 14)
+        title_font = ImageFont.truetype(font_path, 20)
+        
+        text = rtcprof.name
+        width, height = draw.textsize(text, font=title_font)
+        text_offset = (img_width/2 - width/2, top_margin + rtc_height + height/2)
+        draw.text(text_offset, text, font=title_font, fill=text_color)
+        
+        outport_polygon = ((-port_height/2, -port_height/2), (+port_height/2, -port_height/2),
+                           (port_height/2+port_height/2, 0), (port_height/2, port_height/2),
+                           (-port_height/2, port_height/2), (-port_height/2, -port_height/2))
+        
+        for i, p in enumerate(rtcprof.outports):
+            offset = (img_width/2 + rtc_width/2, top_margin + top_bottom_margin + (port_height+port_margin)*i + port_height/2)
+            port_polygon = []
+            for point in outport_polygon:
+                port_polygon.append((point[0] + offset[0], point[1] + offset[1]))
+                pass
+            draw.polygon(port_polygon, fill=fill_color, outline=line_color)
+            
+            text = p.name + '(' + p.type + ')'
+            text_pos = (offset[0] + port_height/2, offset[1] - port_height*1.5)
+            draw.text(text_pos, text, fill=text_color, font=text_font)
+            pass
+
+
+        svcport_polygon = ((-port_height/2, -port_height/2), (+port_height/2+port_height/2, -port_height/2),
+                           (+port_height/2+port_height/2, port_height/2),
+                           (-port_height/2, port_height/2), (-port_height/2, -port_height/2))
+        
+        len_svc = 0
+        i = len(rtcprof.outports) - 1
+        for j, s in enumerate(rtcprof.serviceports):
+            print 'srvports'
+            i = i + 1
+            offset = (img_width/2 + rtc_width/2, top_margin + top_bottom_margin + (port_height+port_margin)*i + port_height/2)
+            
+            for k, f in enumerate(s.serviceInterfaces):
+                i = i + 1
+                start = (offset[0] + port_height/2, offset[1])
+                intf_offset = (img_width/2 + rtc_width/2 + port_height/2 * 5, top_margin + top_bottom_margin + (port_height+port_margin)*i + port_height/2)
+                pivot0 = (start[0] + port_height/2 *2, start[1])
+                pivot1 = (pivot0[0], intf_offset[1])
+                draw.line((start, pivot0), fill=line_color)
+                draw.line((pivot0, pivot1), fill=line_color)
+                draw.line((pivot1, intf_offset), fill=line_color)
+                
+                bbox = ((intf_offset[0], intf_offset[1]-port_height/2),
+                        (intf_offset[0] + port_height, intf_offset[1]+port_height/2))
+                
+                if f.direction == 'Provided':
+                    draw.ellipse(bbox, outline=line_color)
+                else:
+                    draw.arc(bbox, 90, 270, fill=line_color)
+                    pass
+
+                text = f.type
+                text_pos = (intf_offset[0] + port_height/2, intf_offset[1] - port_height*1.5)
+                draw.text(text_pos, text, fill=text_color, font=text_font)
+                pass
+                
+            len_svc = len_svc + len(s.serviceInterfaces)
+
+
+            port_polygon = []
+            for point in svcport_polygon:
+                port_polygon.append((point[0] + offset[0], point[1] + offset[1]))
+                pass
+            draw.polygon(port_polygon, fill=fill_color, outline=line_color)
+            
+            
+            text = s.name
+            text_pos = (offset[0] + port_height/2, offset[1] - port_height*1.5)
+            draw.text(text_pos, text, fill=text_color, font=text_font)
+            
+            
+            
+            inport_polygon = ((-port_height/2-port_height/2, -port_height/2), (+port_height/2, -port_height/2),
+                              (+port_height/2, +port_height/2),
+                              (-port_height/2-port_height/2, port_height/2),
+                              (-port_height/2-port_height/2+port_height/2, 0),
+                              (-port_height/2-port_height/2, -port_height/2))
+            
+            for i, p in enumerate(rtcprof.inports):
+                offset = (img_width/2 - rtc_width/2, top_margin + top_bottom_margin + (port_height+port_margin)*i + port_height/2)
+                port_polygon = []
+                for point in inport_polygon:
+                    port_polygon.append((point[0] + offset[0], point[1] + offset[1]))
+                    pass
+                draw.polygon(port_polygon, fill=fill_color, outline=line_color)
+
+                text = p.name + '(' + p.type + ')'
+                width, height = draw.textsize(text, font=text_font)
+                text_pos = (offset[0] - port_height/2 - width, offset[1] - port_height*1.5)
+                draw.text(text_pos, text, fill=text_color, font=text_font)
+                pass
+        return im
+
+
